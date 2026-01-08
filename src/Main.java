e7289dd2d962c0ec127858e2991545dd8675be83
Erick <berick0935@gmail.com>
2026-01-08 08:22:21 +0800

    init commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
      public static void main(String[] args){
-          System.out.print("Hello World");
+
+          int age = 25;
+          String month = "August";
+          int year = 2000;
+
+          // This is my first java program
+
+          System.out.println("Erick Jay R. Bernal");
+          System.out.println("I love Pizza!");
+          System.out.print(month);
+          System.out.print(" ");
+          System.out.print(age);
+          System.out.print(", ");
+          System.out.println(year);
+
+
      }
 }
