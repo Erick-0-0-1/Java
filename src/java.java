fb546a6cd4e16c68d9813bc0e46c3abf22e8aad3
Erick <berick0935@gmail.com>
2026-01-08 14:07:29 +0800

    init commit

--- src/java.java
+++ src/java.java
@@ -2,12 +2,13 @@ import java.util.Scanner;
 
 
 
-public class java {
+public class java{
     public static void main(String[] args){
 
+        /*
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter your name: ");
+         System.out.print("Enter your name: ");
         String name =   scanner.nextLine();
 
         System.out.print("Enter your age: ");
@@ -25,7 +26,31 @@ public class java {
         System.out.println("Hello " + name);
         System.out.println("You are " + age + " years old");
         System.out.println("Your GPA is: " + gpa );
-        System.out.println("Student: " + isStudent);
+
+        if (isStudent){
+            System.out.println("You are enrolled as a student");
+        }
+        else {
+            System.out.println("You are not enrolled as a student");
+        }
+         */
+
+        double width = 0;
+        double height = 0;
+        double area = 0;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Please insert the width: ");
+        width = scanner.nextDouble();
+
+        System.out.print("Please insert the height: ");
+        height = scanner.nextDouble();
+
+        area = width * height;
+        System.out.print("The area of the Triangle is: " + area + "cm");
+
+
 
         scanner.close();
     }
