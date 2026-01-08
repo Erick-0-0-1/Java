a61c3205032693cb3fc7a1684006e389742fb8ef
Erick <berick0935@gmail.com>
2026-01-08 13:27:12 +0800

    init commit

--- src/java.java
+++ src/java.java
@@ -13,12 +13,19 @@ public class java {
         System.out.print("Enter your age: ");
         int age = scanner.nextInt();
 
+
         System.out.print("What is your GPA: ");
         double gpa = scanner.nextDouble();
+
+        System.out.print("Are you a student (true/false): ");
+        boolean isStudent = scanner.nextBoolean();
+
+        System.out.println(" ");
 
         System.out.println("Hello " + name);
         System.out.println("You are " + age + " years old");
         System.out.println("Your GPA is: " + gpa );
+        System.out.println("Student: " + isStudent);
 
         scanner.close();
     }
