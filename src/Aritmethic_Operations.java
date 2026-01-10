f039ba073028a50d896a9eeaf08d5db3f3af1221
Erick <berick0935@gmail.com>
2026-01-11 07:00:38 +0800

    init commit

--- src/Aritmethic_Operations.java
+++ src/Aritmethic_Operations.java
@@ -8,11 +8,21 @@ public class Aritmethic_Operations {
         Scanner scanner = new Scanner(System.in);
 
         //Augmented Assignment Operators
-
+/*
         int x = 10;
         int y = 5;
 
         x -= y;
+
+        System.out.println(x);
+*/
+
+        //Increment & Decrement Operators
+
+
+        int x = 1;
+
+        x ++;
 
         System.out.println(x);
 
