988cbb4a4524d97dbb02d743a037afa14725adde
Erick <berick0935@gmail.com>
2026-01-15 10:53:16 +0800

    init commit

---
+++ src/Shopping.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Shopping {
+    public static void main(String []args){
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        String[] food = {Burger, Pizza, Icecream};
+        Double[] price = {50.0, 150.0, 500.0};
+        char Pesos;
+        int choice;
+        double total;
+        
+
+
+        System.out.println(food);
+
+
+
+        System.out.println("Hi");
+
+        scanner.close();
+
+
+    }
+}
